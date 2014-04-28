12132b0b92018f64e7cdff2b0f83e1fda09696be
Hadrien Mary <marelo64@gmail.com>
2014-04-28 14:54:53 +0200

    next

--- spindle_tracker/tracking/__init__.py
+++ spindle_tracker/tracking/__init__.py
@@ -1,3 +1,3 @@
 from .tracker import Tracker
-from .cen2_tracker import Cen2Tracker
-from .spb_tracker import SPBTracker
+# from .cen2_tracker import Cen2Tracker
+# from .spb_tracker import SPBTracker

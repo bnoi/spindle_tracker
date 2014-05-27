ede483d1e1c73eff45a6deda7fe61ca92874fb1a
Hadrien Mary <hadrien.mary@gmail.com>
2014-05-27 12:13:16 +0200

    bk

--- spindle_tracker/utils/stats.py
+++ spindle_tracker/utils/stats.py
@@ -11,6 +11,7 @@ def print_stats(data):
     count = groups.count()
     sem = groups.aggregate(sp.stats.sem)
 
+    print("Labels : {}".format(list(groups.groups.keys())))
     print("N : {}".format(count.values.T[0]))
     print("Mean : {}".format(mean.values.T[0]))
     print("Std : {}".format(std.values.T[0]))
@@ -18,6 +19,7 @@ def print_stats(data):
 
     for label1, label2 in itertools.combinations(data.index.unique(), 2):
         print("Kolmogorov-Smirnov test : {} - {}".format(label1, label2))
-        ks_value, p_value = sp.stats.ks_2samp(data.loc[label1].values[0], data.loc[label2].values[0])
+        ks_value, p_value = sp.stats.ks_2samp(data.loc[label1].values[0],
+                                              data.loc[label2].values[0])
         print("\tKS value = {}".format(ks_value))
         print("\tp-value = {:.2e}".format(p_value))
